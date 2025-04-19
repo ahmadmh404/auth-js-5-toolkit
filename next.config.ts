5c55165a530d7f2f48b816e95caf0bc274d83374
Ahmad <hmadhossamo789@gmai.com`>
2025-04-19 10:16:53 +0300

    cancel experimental feature

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  experimental: {
-    nodeMiddleware: true,
-  },
-};
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
