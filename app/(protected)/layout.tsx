a75a025de773af298e737c5a588fce014a41c4b6
Ahmad <hmadhossamo789@gmai.com`>
2025-04-19 16:20:12 +0300

    added reacct-hot-toast

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./_componenets/navbar";
+import { Toaster } from "react-hot-toast";
 
 interface ProtectedLayoutProsp {
   children: React.ReactNode;
@@ -9,6 +10,7 @@ const ProtectedLayout = ({ children }: ProtectedLayoutProsp) => {
     <div className="w-full h-screen flex flex-col gap-y-10 items-center justify-center bg-gradient-to-b from-sky-500 to-blue-700">
       <Navbar />
       {children}
+      <Toaster />
     </div>
   );
 };
--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -2,12 +2,17 @@ import { currentRole } from "@/lib/auth";
 import { UserRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+import { toast } from "react-hot-toast";
+
 export async function GET() {
   const role = await currentRole();
 
   if (role === UserRole.ADMIN) {
+    toast.success("You're Allowed Sir 😎");
     return new NextResponse(null, { status: 200 });
   }
+
+  toast.success("You're not Allowed normal user 😜");
 
   return new NextResponse(null, { status: 403 });
 }
